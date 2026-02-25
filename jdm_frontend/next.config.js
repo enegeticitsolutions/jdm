1e3c0a00dda1511dede96bc530be2da22e2c3358
Dhruv Mehta <mehtadhruv208@gmail.com>
2026-02-25 16:48:40 +0530

    Allow Next.js Image component to load production backend domains

--- jdm_frontend/next.config.js
+++ jdm_frontend/next.config.js
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/media/**',
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "82.112.236.35",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.jdmgroups.com",
+        pathname: "/**",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
